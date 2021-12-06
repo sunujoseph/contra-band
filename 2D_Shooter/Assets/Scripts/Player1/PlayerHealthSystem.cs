8eed4f4653ad773fd5c08fefe62f01e9b0076329
AjB1724 <92698764+AjB1724@users.noreply.github.com>
2021-12-06 12:42:15 -0500

    Test

--- 2D_Shooter/Assets/Scripts/Player1/PlayerHealthSystem.cs
+++ 2D_Shooter/Assets/Scripts/Player1/PlayerHealthSystem.cs
@@ -2,28 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerHealthSystem : MonoBehaviour
 {
     public int maxHealth, currentHealth;
+
+    public GameObject HP1, HP2, HP3;
 
     GameObject healthBar;
     void Start()
     {
         currentHealth = maxHealth;
     }
-
+    
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            switch (currentHealth)
+            {
+                case 3:
+                    HP3.SetActive(true);
+                    HP2.SetActive(false);
+                    HP1.SetActive(false);
+                    break;
+
+                case 2:
+                    HP3.SetActive(false);
+                    HP2.SetActive(true);
+                    HP1.SetActive(false);
+                    break;
+
+                case 1:
+                    HP3.SetActive(false);
+                    HP2.SetActive(false);
+                    HP1.SetActive(true);
+                    break;
+
+                default:
+                    HP3.SetActive(false);
+                    HP2.SetActive(false);
+                    HP1.SetActive(false);
+                    break;
+            }
+        }
     }
 
     public void DamageHealth()
     {
         currentHealth -= 1;
 
+        switch (currentHealth)
+        {
+            case 3:
+                HP3.SetActive(true);
+                HP2.SetActive(false);
+                HP1.SetActive(false);
+                break;
+
+            case 2:
+                HP3.SetActive(false);
+                HP2.SetActive(true);
+                HP1.SetActive(false);
+                break;
+
+            case 1:
+                HP3.SetActive(false);
+                HP2.SetActive(false);
+                HP1.SetActive(true);
+                break;
+
+            default:
+                HP3.SetActive(false);
+                HP2.SetActive(false);
+                HP1.SetActive(false);
+                break;
+        }
+
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
         }
+
+
     }
 }
