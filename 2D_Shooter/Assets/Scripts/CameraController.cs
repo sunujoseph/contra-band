5bc155ed284167313f90d11db3716e66197556b4
sunujoseph <sunupiousjoseph@gmail.com>
2021-12-05 19:17:55 -0500

    Camera Follow Fix

--- 2D_Shooter/Assets/Scripts/CameraController.cs
+++ 2D_Shooter/Assets/Scripts/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] float maxUp, maxDown, maxLeft, maxRight;
 
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,8 @@ public class CameraController : MonoBehaviour
     {
         //Follow target's x position
 
-        transform.position = new Vector3(targetView.position.x, transform.position.y, transform.position.z);
+        transform.position = targetView.position + offset;
+
+        //transform.position = new Vector3(targetView.position.x, transform.position.y, transform.position.z);
     }
 }
